091c68e332672c336bb41878747e8c850c4d8b4d
jamhour1g <yazanjam50@gmail.com>
2024-01-19 18:22:26 +0200

    Create basic controller for addStudent-dialog.fxml

---
+++ src/main/java/com/jamhour/educationhub/controllers/admin/AddStudentDialog.java
@@ -0,0 +1,19 @@
+package com.jamhour.educationhub.controllers.admin;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class AddStudentDialog {
+
+
+    @FXML
+    private TextField idTextField;
+    @FXML
+    private TextField nameTextField;
+    @FXML
+    private TextField phoneTextField;
+    @FXML
+    private TextField emailTextField;
+
+
+}
