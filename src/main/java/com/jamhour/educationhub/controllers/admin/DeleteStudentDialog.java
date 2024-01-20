ff92e681ba8a3be50ba31af8ce84caefb7fa5895
jamhour1g <yazanjam50@gmail.com>
2024-01-21 00:30:45 +0200

    Add method to delete students via dialog

--- src/main/java/com/jamhour/educationhub/controllers/admin/DeleteStudentDialog.java
+++ src/main/java/com/jamhour/educationhub/controllers/admin/DeleteStudentDialog.java
@@ -2,8 +2,13 @@ package com.jamhour.educationhub.controllers.admin;
 
 import atlantafx.base.controls.Message;
 import atlantafx.base.theme.Styles;
+import com.jamhour.data.Student;
+import com.jamhour.database.Schema;
+import com.jamhour.database.queries.Queries;
+import com.jamhour.educationhub.controllers.ControllerResource;
 import javafx.fxml.FXML;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.stage.Window;
 
 public class DeleteStudentDialog {
 
@@ -17,4 +22,52 @@ public class DeleteStudentDialog {
         message.getStyleClass().add(Styles.ACCENT);
     }
 
+    public static void showDeleteStudentDialog(Window owner) {
+        Dialog<ButtonType> dialog = new Dialog<>();
+        dialog.setTitle("Delete Student");
+        dialog.setContentText("Enter the ID of the student you want to delete.");
+        dialog.initOwner(owner);
+        dialog.setDialogPane((DialogPane) ControllerResource.ADMIN_DELETE_STUDENT_DIALOG.getContent());
+
+        dialog.showAndWait()
+                .ifPresent(buttonType -> {
+                    if (buttonType == ButtonType.OK) {
+                        DeleteStudentDialog content = ControllerResource.ADMIN_DELETE_STUDENT_DIALOG.getController();
+                        content.deleteStudent();
+                    } else {
+                        dialog.close();
+                    }
+                });
+    }
+
+    private void deleteStudent() {
+        int parsedId;
+        try {
+            parsedId = Integer.parseInt(studentId.getText());
+        } catch (NumberFormatException e) {
+            showErrorOnInvalidInput("Invalid ID.", "Please enter a valid ID.");
+            return;
+        }
+
+        if (studentId.getText() == null || studentId.getText().isBlank()) {
+            showErrorOnInvalidInput("Enter a value.", "Please enter the id you want to delete");
+            return;
+        }
+        Queries.deleteFromTableUsing(Schema.Tables.STUDENT, Student.Column.ID, parsedId);
+    }
+
+    private void showErrorOnInvalidInput(String title, String description) {
+
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(description);
+        alert.showAndWait().ifPresent(buttonType -> {
+            if (buttonType == ButtonType.OK) {
+                showDeleteStudentDialog(null);
+            } else {
+                alert.close();
+            }
+        });
+    }
 }
